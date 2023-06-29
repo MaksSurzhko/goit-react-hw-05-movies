2431f839d5b5dcdfc9962dfddc2e7f29d97130e2
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-06-29 20:05:31 +0300

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -17,7 +17,7 @@ export const HeaderStyled = styled.header`
   color: #fff;
   cursor: pointer;
   text-decoration: none;
-  background-color: #00d4ff;
+  background-color: gray;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
