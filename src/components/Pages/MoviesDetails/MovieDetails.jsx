0b3b738f96b70f9de0fe8c6eb46c154be630aebe
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-06-29 19:59:37 +0300

    Update MovieDetails.jsx

--- src/components/Pages/MoviesDetails/MovieDetails.jsx
+++ src/components/Pages/MoviesDetails/MovieDetails.jsx
@@ -1,3 +1,86 @@
+// import { useEffect, useState } from 'react';
+// import { getDetailMovie } from 'components/Api/Api';
+// import { Loader } from 'components/Loader/Loader';
+// import { Outlet, useParams, Link } from 'react-router-dom';
+
+// const MovieDetails = () => {
+//   const { movieId } = useParams();
+//   const [movie, setMovie] = useState([]);
+//   const [genres, setGenres] = useState([]);
+//   const [date, setDate] = useState('');
+//   const [urlImage, setUrlImage] = useState('');
+//   const [error, setError] = useState(null);
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   useEffect(() => {
+//     const fetchDetalsMovie = async () => {
+//       try {
+//         setIsLoading(true)
+//         const movie = await getDetailMovie(movieId);
+//         const genres = movie.genres;
+//         const date = movie.release_date.split('-')[0];
+//         const url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+//         if (movie === []) {
+//           setError(`Інформація по фільму відсутня`);
+//         } else {
+//           setGenres(genres);
+//           setMovie(movie);
+//           setDate(date);
+//           setUrlImage(url);
+//         }
+//         // setGenres(genres);
+//         // setMovie(movie);
+//         // setDate(date);
+//         // setUrlImage(url);
+//       } catch (error) {
+//         setError(error.message);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     };
+//     fetchDetalsMovie();
+//   }, [movieId]);
+
+//   const { title, overview, vote_average } = movie;
+//   console.log('movie', movie);
+
+//   return (
+//     <section>
+//       {error && <p>Sorry, something went wrong</p>}
+//       {isLoading && <Loader />}
+//       <div>
+//         <img src={urlImage} alt={title}></img>
+//         <div>
+//           <h1>
+//             {title}
+//             <span>{date}</span>
+//           </h1>
+//           <p>User Score:{(vote_average * 10).toFixed(0)} %</p>
+//           <h2>Overview</h2>
+//           <p>{overview}</p>
+//           <h3>Genres</h3>
+//           <p>{genres.map(el => el.name).join(' / ')}</p>
+//         </div>
+//       </div>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="cast">Cast</Link>
+//           </li>
+//           <li>
+//             <Link to="reviews">Reviews</Link>
+//           </li>
+//         </ul>
+//         <Outlet />
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default MovieDetails;
+
+
 import { useEffect, useState } from 'react';
 import { getDetailMovie } from 'components/Api/Api';
 import { Loader } from 'components/Loader/Loader';
@@ -13,42 +96,42 @@ const MovieDetails = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchDetalsMovie = async () => {
+    const fetchDetailsMovie = async () => {
       try {
-        setIsLoading(true)
+        setIsLoading(true);
         const movie = await getDetailMovie(movieId);
         const genres = movie.genres;
         const date = movie.release_date.split('-')[0];
         const url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
         if (movie === []) {
-          setError(`Інформація по фільму відсутня`);
+          setError('Інформація по фільму відсутня');
         } else {
           setGenres(genres);
           setMovie(movie);
           setDate(date);
           setUrlImage(url);
         }
-        // setGenres(genres);
-        // setMovie(movie);
-        // setDate(date);
-        // setUrlImage(url);
       } catch (error) {
         setError(error.message);
       } finally {
         setIsLoading(false);
       }
     };
-    fetchDetalsMovie();
+    fetchDetailsMovie();
   }, [movieId]);
 
   const { title, overview, vote_average } = movie;
-  console.log('movie', movie);
+
+  const handleGoBack = () => {
+    window.history.back(); // Go back to the previous page
+  };
 
   return (
     <section>
       {error && <p>Sorry, something went wrong</p>}
       {isLoading && <Loader />}
+      <button onClick={handleGoBack}>Go Back</button>
       <div>
         <img src={urlImage} alt={title}></img>
         <div>
@@ -56,7 +139,7 @@ const MovieDetails = () => {
             {title}
             <span>{date}</span>
           </h1>
-          <p>User Score:{(vote_average * 10).toFixed(0)} %</p>
+          <p>User Score: {(vote_average * 10).toFixed(0)}%</p>
           <h2>Overview</h2>
           <p>{overview}</p>
           <h3>Genres</h3>
@@ -74,6 +157,7 @@ const MovieDetails = () => {
         </ul>
         <Outlet />
       </div>
+      
     </section>
   );
 };
